7d8abd313e70aa0cde04e489c8ab367aa72d6bcb
Talia Seada <78349342+TaliaSeada@users.noreply.github.com>
2022-05-26 12:17:55 +0300

    Add files via upload

---
+++ measure.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ sender.c
@@ -0,0 +1,52 @@
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <netinet/tcp.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <unistd.h> 
+
+int main(int argc, char **argv) {
+    // open a TCP socket
+    char buf[256];
+    socklen_t len;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+ 	if (sock == -1) {
+ 		perror("socket");
+        return -1;
+    } 
+
+    len = sizeof(buf); 
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) { 
+        perror("getsockopt");
+        return -1;
+    } 
+
+    printf("Current: %s\n", buf); 
+
+    // connect with measure.c
+
+
+    // send the 1gb.txt file
+
+
+    // change the CC
+    strcpy(buf, "reno"); 
+    len = strlen(buf);
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0) {
+        perror("setsockopt"); 
+        return -1;
+    }
+    len = sizeof(buf); 
+    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0) {
+        perror("getsockopt"); 
+        return -1; 
+    } 
+    printf("New: %s\n", buf); 
+
+    // send the 1gb.txt file
+
+
+    // close the socket
+    close(sock); 
+    return 0; 
+}
